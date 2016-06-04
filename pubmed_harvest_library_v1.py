d5e0cf543030e848b961d160038f3fcfcfdbb6f4
ebenjoseph <ebenjoseph@gmail.com>
2016-06-04 15:27:42 -0700

    library v2

--- pubmed_harvest_library_v1.py
+++ pubmed_harvest_library_v1.py
@@ -21,7 +21,7 @@ from multiprocessing.dummy import Pool as ThreadPool # for multithreading
 #Create a second handler, change formatting, and appends it to the root to stream log to console
 #set logging level from command line. python.py -log=INFO
 root = logging.getLogger()
-logging.basicConfig(level=logging.INFO, filename='pubmed_harvest_v4.log', format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(level=logging.INFO, filename='pubmed_harvest_library.log', format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 streamthelog = logging.StreamHandler(sys.stdout)
 streamthelog.setLevel(logging.DEBUG)
 streamthelog.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
